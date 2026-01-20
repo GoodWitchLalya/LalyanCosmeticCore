7685db98b5ab6bae302fa9e1c3cb38c4f2f98195
GoodWitchLalya <72929397+GoodWitchLalya@users.noreply.github.com>
2026-01-20 15:49:11 +0100

    Commented

--- src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/command/CosmeticReloadCommand.java
+++ src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/command/CosmeticReloadCommand.java
@@ -13,18 +13,32 @@ import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
+/**
+ * Subcommand to manually trigger a reload of all cosmetic assets.
+ * Requires OP permissions.
+ */
 public class CosmeticReloadCommand extends AbstractPlayerCommand {
     
     private final Universe universe = Universe.get();
     
+    /**
+     * Constructor for the 'reload' subcommand.
+     * Defines the command's description and permissions.
+     */
     public CosmeticReloadCommand() {
         super("reload", "Manually reloads cosmetics");
         this.setPermissionGroups("OP");
     }
     
+    /**
+     * Executes the command logic.
+     * Triggers the asset reloading process and sends the log output to the player.
+     */
     @Override
     protected void execute(@NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
+        // Execute the reload on the world's main thread.
         universe.getWorld(playerRef.getWorldUuid()).execute(() -> {
+            // Call the FileManager to reload assets and send the resulting log to the command sender.
             commandContext.sendMessage(Message.raw(FileManager.wakeUp()));
         });
     }
