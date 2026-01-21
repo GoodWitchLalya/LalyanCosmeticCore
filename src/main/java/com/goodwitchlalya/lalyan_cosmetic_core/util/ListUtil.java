0426aa3bf023be67406b6b75220b0b3152e2bdf0
GoodWitchLalya <72929397+GoodWitchLalya@users.noreply.github.com>
2026-01-21 17:02:27 +0100

    Commented

--- src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/util/ListUtil.java
+++ src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/util/ListUtil.java
@@ -3,7 +3,12 @@ package com.goodwitchlalya.lalyan_cosmetic_core.util;
 import java.util.ArrayList;
 import java.util.List;
 
+// Utility class for list operations.
 public class ListUtil {
+    // Converts an immutable list to a mutable ArrayList.
+    // @param list The original list, which may be immutable.
+    // @param <T> The type of elements in the list.
+    // @return A new ArrayList containing all elements of the original list.
     public static<T> List<T> mutable(List<T> list) {
         return new ArrayList<>(list);
     }
