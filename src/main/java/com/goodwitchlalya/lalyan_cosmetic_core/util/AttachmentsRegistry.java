32d248d4cc9434ce405ce6a509301a6211668607
MtcLeo05 <leonardo.matteucci1805@gmail.com>
2026-01-24 19:51:44 +0100

    fix being unable to use skin gradient

--- src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/util/AttachmentsRegistry.java
+++ src/main/java/com/goodwitchlalya/lalyan_cosmetic_core/util/AttachmentsRegistry.java
@@ -183,8 +183,6 @@ public class AttachmentsRegistry {
         Yellow
     }
     
-    ;
-    
     private static final GradientSet eyesGradientSet = new GradientSet("Eyes_Gradient");
     
     public enum eyesGradientSetNames {
@@ -397,8 +395,8 @@ public class AttachmentsRegistry {
         s11, s12, s13, s14, s15, s16, s17, s18, s19, s20,
         s21, s22, s23, s24, s25, s26, s27, s28, s29, s30,
         s31, s32, s33, s34, s35, s36, s37, s38, s39, s40,
-        s41, s42, s43, s44, s45, s46, s47, s48, s49, s50,
-        s51, s52;
+        s41, s42, s43, s44, s45, s46, s47, /*s48, s49, s50,
+        s51, s52*/;
         
         public String getName() {
             return this.name().replace("s", "");
